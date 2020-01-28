097b43e2f591867760101ba05faee05c777d5a29
winChawakorn <chawakorn.su@ku.th>
2020-01-28 16:44:48 +0700

    Add bursty, qa, facebook sections

--- src/components/Newspaper.js
+++ src/components/Newspaper.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { NewspaperContainer, NewspaperContent, FlexWrap, NewspaperDate, NewspaperImage } from '../styles'
+import { NewspaperContainer, NewspaperContent, Flex, FlexWrap, NewspaperDate, NewspaperImage, NewspaperQA, NewspaperFacebook } from '../styles'
 import { DatePicker } from 'antd'
 import axios from 'axios'
 import moment from 'moment'
@@ -14,6 +14,8 @@ const mockTopics = [
   { topic_id: '38602203', viewers: 26649 },
   { topic_id: '38610945', viewers: 18863 },
   { topic_id: '38622192', viewers: 15804 },
+  { topic_id: '39580655', viewers: 15804 },
+  { topic_id: '39587022', viewers: 15804 },
 ]
 
 export default () => {
@@ -32,17 +34,28 @@ export default () => {
         values.forEach(value => {
           const topic = mockTopics.find(t => t.topic_id === value.data._id)
           const data = value.data._source
-          _topics.push({ ...topic, url: data.permalink, title: data.title_full, desc: data.desc, image: findImage(data.desc_full) })
+          _topics.push({
+            ...topic,
+            ...data,
+            url: data.permalink,
+            title: data.title_full,
+            desc: data.desc,
+            image: findImage(data.desc_full)
+          })
         })
         _topics.sort((a, b) => b.viewers - a.viewers)
         setTopics(_topics)
       })
   }, [])
 
+  const cut = (desc, length) => {
+    return desc.substring(0, length).concat(desc.length <= length ? '' : '...')
+  }
+
   const getNewspaperContent = (layout = 1, topic = {}) => {
     const hasImage = topic.image
     const length = layout > 1 && hasImage ? 200 : 300
-    const desc = topic.desc.substring(0, length).concat(topic.desc.length <= length ? '' : '...')
+    const desc = cut(topic.desc, length)
     const title = <h1>{topic.title}</h1>
     const detail = (
       <p>
@@ -55,7 +68,7 @@ export default () => {
 
     return (
       <NewspaperContent layout={layout} hasImage={hasImage}>
-        {layout < 3 ? (
+        {layout < 2 ? (
           <>
             <div>
               {title}
@@ -74,24 +87,90 @@ export default () => {
     )
   }
 
+  const getQAContent = (topic = {}) => {
+    return (
+      <NewspaperQA>
+        <div>
+          <h1>Q: {topic.title} {topic.title.includes('?') ? '' : '?'}</h1>
+          <p><img src={topic.avatar} alt={topic.nickname} height="40" /> {topic.nickname} | {moment(parseInt(topic.created_time, 10) * 1000).format('LLL')}</p>
+        </div>
+        <div>
+          A: {cut(topic.comments_desc[0].substring(9), 300)}
+          <br />
+          <br />
+          <p><img src={topic.comments[0].avatar} alt={topic.comments[0].nickname} height="40" />{topic.comments[0].nickname} | {moment(parseInt(topic.comments[0].created_time, 10) * 1000).format('LLL')}</p>
+        </div>
+      </NewspaperQA>
+    )
+  }
+
+  const getFacebookContent = (topic = {}) => {
+    return (
+      <NewspaperFacebook>
+        <Flex>
+          <img src={topic.avatar} alt={topic.nickname} height="40" />
+          <div>
+            <a rel="noopener noreferrer" target="_blank" href={topic.url}>{topic.nickname}</a>
+            <br />
+            <p>
+              {moment(parseInt(topic.created_time, 10) * 1000).format('LLL')}
+            </p>
+          </div>
+        </Flex>
+        {cut(topic.desc, 300)}<br /><a rel="noopener noreferrer" target="_blank" href={topic.url}>ดูเพิ่มเติม</a>
+        {topic.image ? <img src={topic.img} alt="" /> : ''}
+      </NewspaperFacebook>
+    )
+  }
+
   return topics.length > 0 ? (
     <NewspaperContainer>
       <h1>P A N T I P&nbsp;&nbsp;&nbsp;N E W S</h1>
       <NewspaperDate>
-        <p>ปีที่ 1 ฉบับที่ 1</p>
+        <p>ปีที่ 1 ฉบับที่ 1 ประจำวันที่</p>
         <RangePicker
           style={{ width: `300px` }}
           defaultValue={[moment('04-03-2019', dateFormat), moment('06-03-2019', dateFormat)]}
         />
       </NewspaperDate>
       <hr />
-      <FlexWrap>
-        {getNewspaperContent(1, topics[2])}
-        {getNewspaperContent(3, topics[4])}
-        {getNewspaperContent(3, topics[0])}
-        {getNewspaperContent(3, topics[1])}
-        {getNewspaperContent(3, topics[3])}
-      </FlexWrap>
+      <hr />
+      <Flex>
+        <div>
+          <FlexWrap>
+            {getNewspaperContent(1, topics[2])}
+            {getNewspaperContent(3, topics[4])}
+            {getNewspaperContent(3, topics[0])}
+            {getNewspaperContent(3, topics[1])}
+            {getNewspaperContent(3, topics[3])}
+            {getNewspaperContent(3, topics[3])}
+            {getNewspaperContent(3, topics[3])}
+          </FlexWrap>
+        </div>
+        <div>
+          <h1>Trending now</h1>
+          {[0, 1, 2, 3].map(n => getNewspaperContent(2, topics[n]))}
+        </div>
+      </Flex>
+      <hr />
+      <hr />
+      <Flex>
+        <div>
+          <h1>คำถามยอดฮิต</h1>
+          <FlexWrap>
+            {getQAContent(topics[5])}
+            {getQAContent(topics[6])}
+            {getQAContent(topics[5])}
+          </FlexWrap>
+        </div>
+        <div>
+          <h1>Hits on Facebook</h1>
+          <FlexWrap>
+            {getFacebookContent(topics[5])}
+            {getFacebookContent(topics[6])}
+          </FlexWrap>
+        </div>
+      </Flex>
     </NewspaperContainer>
   )
     : <div>Loading...</div>
--- src/styles.js
+++ src/styles.js
@@ -41,7 +41,7 @@ export const NavbarContainer = styled.div`
   height: 80px;
   background-color: white;
   color: black;
-  box-shadow: ${props => props.scrollPosition > 0 ? '0px 2px 5px rgba(0, 0, 0, 0.25)' : '0px 2px 5px rgba(0, 0, 0, 0.09)'};
+  box-shadow: ${props => props.scrollPosition > 0 ? '0px 2px 10px rgba(0, 0, 0, 0.3)' : '0px 2px 5px rgba(0, 0, 0, 0.09)'};
   display: flex;
   align-items: center;
   position: fixed;
@@ -54,11 +54,15 @@ export const NavbarContainer = styled.div`
     animation: ${SlideIn('100%')} 2s;
     animatin: ${FadeIn} 2s;
     margin: 0 5%;
-    ${'' /* overflow: auto; */}
   }
 
   & > div:first-child {
     margin: 0;
+  }
+
+  & > div:nth-child(2) > div:last-child {
+    position: absolute;
+    right: 4%;
   }
 
   & > div {
@@ -124,18 +128,16 @@ export const SideBarRoom = styled.span`
 `
 
 export const NewspaperContainer = styled.div`
-  background: white;
   max-width: 90%;
   margin: auto;
   min-height: 100vh;
   padding: 5px;
-  color: black;
   animation: ${FadeIn} 1s;
 
   & > h1 {
     text-align: center;
     margin-top: 20px;
-    margin-bottom: 0px;
+    margin-bottom: 10px;
     font-size: 70px;
     font-weight: bold;
     font-family: 'Anton', sans-serif;
@@ -143,10 +145,54 @@ export const NewspaperContainer = styled.div`
 
   & > hr {
     width: 100%;
-    margin-top: 40px;
-    margin-bottom: 20px;
-    border-width: 2px;
+    margin-top: 50px;
+    border-width: 0.5px;
     border-color: black;
+  }
+
+  & > hr:nth-child(4), & > hr:nth-child(7) {
+    margin-top: -5px;
+    margin-bottom: 20px;
+  }
+
+  & > div:nth-child(5) > div:first-child {
+    max-width: 70%;
+    border-right: 1px solid black;
+  }
+
+  & > div:nth-child(5) > div:nth-child(2) > h1, & > div:nth-child(8) > div > h1 {
+    font-family: 'Anton', sans-serif;
+    border: 2px solid black;
+    width: 180px;
+    text-align: center;
+    margin: auto;
+    margin-bottom: 10px;
+  }
+
+  & > div:nth-child(8) > div > h1 {
+    width: 200px;
+    font-family: 'Pridi', serif;
+    font-size: 30px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  & > div:nth-child(8) > div:first-child {
+    width: 50%;
+    border-right: 1px solid black;
+  }
+
+  & > div:nth-child(8) > div {
+    width: 50%;
+  }
+
+  & > div:nth-child(8) > div:nth-child(2) {
+    ${'' /* background-color: #e9ebee; */}
+    padding: 0 20px;
+  }
+
+  & > div:nth-child(8) > div:nth-child(2) > h1 {
+    width: 280px;
   }
 `
 
@@ -154,6 +200,7 @@ export const NewspaperDate = styled.div`
   display: flex;
   font-weight: bold;
   float: right;
+  color: black;
 
   & > p {
     font-size: 20px;
@@ -165,16 +212,17 @@ export const NewspaperDate = styled.div`
 export const NewspaperContent = styled.div`
   width: ${props => {
     switch (props.layout) {
-      case 2: return '66.66%'
+      // case 2: return '66.66%'
       case 3: return '33.33%'
       default: return '100%'
     }
   }};
   padding: 10px 20px;
-  display: ${props => props.layout < 3 ? 'flex' : 'block'};
+  display: ${props => props.layout === 1 ? 'flex' : 'block'};
+  color: black;
 
   & > div:first-child {
-    width: ${props => props.hasImage && props.layout < 2 ? '40%' : '100%'};
+    width: ${props => props.hasImage && props.layout === 1 ? '40%' : '100%'};
   }
 
   & > div > h1, & > div > p {
@@ -194,7 +242,7 @@ export const NewspaperContent = styled.div`
   }
 
   & > div > p {
-    font-size: 16px;
+    font-size: ${props => props.layout === 1 ? '20px' : '16px'};
     font-family: 'Bai Jamjuree', sans-serif;
   }
 `
@@ -203,13 +251,71 @@ export const NewspaperImage = styled.div`
   background-image: url('${props => props.src}');
   background-repeat: no-repeat;
   background-size: 100% auto;
-  width: ${props => props.layout < 2 ? '60%' : '100%'};
-  height: ${props => props.layout < 2 ? 'auto' : '250px'};
+  width: ${props => props.layout === 1 ? '60%' : '100%'};
+  height: ${props => props.layout === 1 ? 'auto' : '200px'};
   margin-top: 10px;
   margin-bottom: 10px;
   transition: transform .2s;
 
   &:hover {
     transform: scale(1.03);
+  }
+`
+
+export const NewspaperQA = styled.div`
+  padding: 10px 20px;
+  color: black;
+
+  & > div:first-child {
+    background-color: #c4c4c4;
+  }
+
+  & > div:first-child > h1 {
+    font-weight: bold;
+    font-size: 20px;
+  }
+
+  & > div:nth-child(2) {
+    background-color: #eeeeee;
+  }
+
+  & > div {
+    font-size: 18px;
+    padding: 10px 10px;
+  }
+
+  & > div > p {
+    text-align: right;
+    font-size: 16px;
+    margin-bottom: 0;
+  }
+
+  & img {
+    border-radius: 50%;
+    margin-right: 8px;
+  }
+`
+
+export const NewspaperFacebook = styled.div`
+  padding: 12px;
+  font-size: 16px;
+  background: white;
+  color: black;
+  margin: 5px 0;
+  border-radius: 3px;
+
+  & > div > div > a {
+    color: #385898;
+    font-weight: 600;
+  }
+
+  & > div > div > p {
+    color: #616770;
+    font-size: 14px;
+  }
+
+  & > div > img {
+    margin-right: 8px;
+    border-radius: 50%;
   }
 `
